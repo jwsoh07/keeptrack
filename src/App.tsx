42795927b7bc7e94dcaa0685f589554d13a76b4f
Soh Jun Wen <121473009+jwsoh07@users.noreply.github.com>
2023-12-13 23:17:04 +0800

    Add routes for HomePage and ProfilePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+
+import HomePage from './home/HomePage';
 import ProjectsPage from './projects/ProjectsPage';
 
 import './App.css';
 
 function App() {
   return (
-    <div className="container">
-      <ProjectsPage />
-    </div>
+    <Router>
+      <header className="sticky row">
+        <span className="logo">
+          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+        </span>
+        <NavLink to="/" className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects" className="button rounded">
+          Projects
+        </NavLink>
+      </header>
+      <div className="container">
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/projects' element={<ProjectsPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
